b23c703ad42f8c2f2ad69aa0941f1dfd8284a65c
Junyoungs7 <junyoungs7@naver.com>
2022-08-18 03:14:05 +0900

    220818

--- src/main/java/com/saehan/shop/service/posts/PostsService.java
+++ src/main/java/com/saehan/shop/service/posts/PostsService.java
@@ -1,4 +1,20 @@
 package com.saehan.shop.service.posts;
 
+import com.saehan.shop.domain.posts.PostsRepository;
+import com.saehan.shop.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
---
+++ src/test/java/com/saehan/shop/web/PostApiControllerTest.java
@@ -0,0 +1,66 @@
+package com.saehan.shop.web;
+
+import com.saehan.shop.domain.posts.Posts;
+import com.saehan.shop.domain.posts.PostsRepository;
+import com.saehan.shop.web.dto.PostsSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
+class PostApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception{
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록() throws Exception{
+        //given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto postsSaveRequestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("me")
+                .build();
+
+        String url = "http://localhost:"+port+"/api/v1/posts";
+
+        //when
+
+        ResponseEntity<Long> responseEntity = testRestTemplate.postForEntity(url, postsSaveRequestDto, Long.class);
+
+        //then
+
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
+        Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+}
